453ed539ca76462e9b0d99fd6e37ad095e247a65
kamal kishore rajpurohit <kkraj16@gmail.com>
2020-10-31 23:29:00 +0530

    Initial commit

---
+++ src/test/java/com/kkrajpurohit007/main/SpringbootPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkrajpurohit007.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
